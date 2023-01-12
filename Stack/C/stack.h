32e2930b4e58899f1980cb57d9531cdd8509ff78
Steven Mead <116687374+stmead23@users.noreply.github.com>
2023-01-12 14:50:02 -0600

    Update stack.h

--- Stack/C/stack.h
+++ Stack/C/stack.h
@@ -1,18 +1,18 @@
 #ifndef STACK_H
 #define STACK_H
-#include <stdbool.h>
+#include <stdbool.h> //Includes the bool values
 
-int * stack;
-int size;
-int max_size;
+int * stack; //Holds the stack elements
+int size; //Current size of the stack
+int max_size; //Sets the max size from the input
 
-void setup(int s);
-int getSize();
-void incSize();
-void decSize();
-bool isEmpty();
-int top();
-void push(int data);
-int pop();
+void setup(int s); //Initializes the stack, and sets max_value and current size to 0
+int getSize(); //returns the current size of the stack
+void incSize(); //increments the size of the stack by 1
+void decSize(); //decrements the size of the stack by 1
+bool isEmpty(); //Returns true if there's no elements in the stack
+int top(); //Returns the first value of the stack
+void push(int data); //If the stack is full, nothing happens. Otherwise, it adds the element to the stack and increments size
+int pop(); //If the stack is empty, nothing is done. Otherwise, it removes the most recent element and returns it
 
 #endif
